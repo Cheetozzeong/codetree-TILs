ac0fd6f5c7ab370c25b787d9617059a05341cd55
Cheetozzeong <81691407+Cheetozzeong@users.noreply.github.com>
2023-11-14 11:37:17 +0900

    Title: 정수 입력받아 계산, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231114/정수 입력받아 계산/calculation-by-inputting-an-integer.java
@@ -0,0 +1,10 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        System.out.print(a*2+3);
+    }
+}
