66c01c46e08b4330be6f3a5627b59b6b958ea693
Cheetozzeong <81691407+Cheetozzeong@users.noreply.github.com>
2023-11-15 17:01:51 +0900

    Title: 짝수만 출력, Time: 82ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 231115/짝수만 출력/output-only-even.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+
+        while(true){
+            if(a==b) break;
+            else {
+                if(a%2 == 0) System.out.print(a);
+            }
+            a++;
+        }
+    }
+}
