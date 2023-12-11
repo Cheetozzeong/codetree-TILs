45ad4bdc5bcc818fb7c718d18ac9e650bdf728a8
Cheetozzeong <81691407+Cheetozzeong@users.noreply.github.com>
2023-12-11 17:48:39 +0900

    Title: 두 개의 직각삼각형, Time: 93ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231211/두 개의 직각삼각형/two-right-triangle.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            for (int k = 0; k < 2*(n-i); k++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
