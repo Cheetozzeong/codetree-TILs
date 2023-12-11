f2f84805e9c3fe5adb1e8d3b05a032f0b0f72ce2
Cheetozzeong <81691407+Cheetozzeong@users.noreply.github.com>
2023-12-11 15:29:25 +0900

    Title: 모두 3의 배수, Time: 86ms, Memory: 8MB, Status: Passed - CodeTree

--- 231211/모두 3의 배수/all-multiples-of-3.java
+++ 231211/모두 3의 배수/all-multiples-of-3.java
@@ -5,13 +5,15 @@ public class Main {
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        boolean flag = false;
         for(int i =0; i<5; i++) {
             if((Integer.parseInt(br.readLine())%3)!=0) {
-                System.out.print(0);
+                flag = false;
                 break;
-            }
+            } else flag=true;
         }
 
-        System.out.print(1); 
+        if(flag) System.out.print(1);
+        else System.out.print(0); 
     }
 }
